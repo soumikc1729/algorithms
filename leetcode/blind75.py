14321387a1f6113712d7b1568fa6f2acaacabad1
Soumik Chatterjee <soumikc1729@gmail.com>
2025-08-01 00:34:37 +0530

    feat(leetcode): solve 'Invert Binary Tree'

--- leetcode/blind75.py
+++ leetcode/blind75.py
@@ -1,3 +1,93 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    @staticmethod
+    def create(val):
+        return TreeNode(val) if val else None
+
+    @staticmethod
+    def deserialize(vals):
+        if not vals:
+            return []
+
+        root = TreeNode.create(vals[0])
+
+        q = deque()
+        q.append(root)
+
+        i = 0
+        while True:
+            node = q.popleft()
+            i += 1
+            if i == len(vals):
+                break
+            node.left = TreeNode.create(vals[i])
+            if node.left:
+                q.append(node.left)
+            i += 1
+            if i == len(vals):
+                break
+            node.right = TreeNode.create(vals[i])
+            if node.right:
+                q.append(node.right)
+
+        return root
+
+    @staticmethod
+    def serialize(root):
+        if not root:
+            return []
+
+        q = deque()
+        q.append(root)
+
+        arr = [root.val]
+
+        def append(node):
+            if node:
+                q.append(node)
+                arr.append(node.val)
+            else:
+                arr.append(None)
+
+        while q:
+            node = q.popleft()
+            append(node.left)
+            append(node.right)
+
+        i = len(arr) - 1
+        while i >= 0 and not arr[i]:
+            i -= 1
+
+        return arr[: i + 1]
+
+
+def invert_tree(root):
+    """
+    >>> TreeNode.serialize(invert_tree(TreeNode.deserialize([4, 2, 7, 1, 3, 6, 9])))
+    [4, 7, 2, 9, 6, 3, 1]
+    >>> TreeNode.serialize(invert_tree(TreeNode.deserialize([2, 1, 3])))
+    [2, 3, 1]
+    >>> TreeNode.serialize(invert_tree(TreeNode.deserialize([])))
+    []
+    """
+    if not root:
+        return root
+
+    left = invert_tree(root.left)
+    right = invert_tree(root.right)
+
+    root.left, root.right = right, left
+
+    return root
+
+
 # https://leetcode.com/problems/contains-duplicate
 def contains_duplicate(nums):
     """
