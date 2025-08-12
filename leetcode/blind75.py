19884693d6123498b7199987e7033659e5ef2a5b
Soumik Chatterjee <soumikc1729@gmail.com>
2025-08-12 22:46:12 +0530

    feat(leetcode): solve 'Set Matrix Zeroes'

--- leetcode/blind75.py
+++ leetcode/blind75.py
@@ -1,3 +1,34 @@
+# https://leetcode.com/problems/set-matrix-zeroes
+def set_zeroes(matrix):
+    """
+    >>> matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    >>> set_zeroes(matrix)
+    >>> matrix
+    [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+    >>> matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+    >>> set_zeroes(matrix)
+    >>> matrix
+    [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
+    """
+    rows, cols = len(matrix), len(matrix[0])
+    row0_has_zero = any(matrix[0][c] == 0 for c in range(cols))
+    col0_has_zero = any(matrix[r][0] == 0 for r in range(rows))
+    for r in range(1, rows):
+        for c in range(1, cols):
+            if matrix[r][c] == 0:
+                matrix[r][0] = matrix[0][c] = 0
+    for r in range(1, rows):
+        for c in range(1, cols):
+            if matrix[r][0] == 0 or matrix[0][c] == 0:
+                matrix[r][c] = 0
+    if row0_has_zero:
+        for c in range(cols):
+            matrix[0][c] = 0
+    if col0_has_zero:
+        for r in range(rows):
+            matrix[r][0] = 0
+
+
 # https://leetcode.com/problems/palindromic-substrings
 def count_substrings(string):
     """
