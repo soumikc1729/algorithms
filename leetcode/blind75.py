3fda25c43d94ffd23fa57ec9ecd9515213d57bf7
Soumik Chatterjee <soumikc1729@gmail.com>
2025-08-24 11:25:44 +0530

    feat(leetcode): solve 'Meeting Rooms'

--- leetcode/blind75.py
+++ leetcode/blind75.py
@@ -1,3 +1,18 @@
+# https://leetcode.com/problems/meeting-rooms
+def can_attend_meetings(intervals):
+    """
+    >>> can_attend_meetings([[0, 30], [5, 10], [15, 20]])
+    False
+    >>> can_attend_meetings([[7, 10], [2, 4]])
+    True
+    """
+    intervals.sort()
+    for i in range(1, len(intervals)):
+        if intervals[i - 1][1] > intervals[i][0]:
+            return False
+    return True
+
+
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 def max_profit(prices):
     """
