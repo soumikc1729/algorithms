b31544670ff0dbd8b6cc9fce0154bf24cbe826b9
Soumik Chatterjee <soumikc1729@gmail.com>
2025-08-30 12:48:18 +0530

    feat(potd): solve 'Valid Sudoku'

--- leetcode/potd.py
+++ leetcode/potd.py
@@ -1,3 +1,62 @@
+from typing import List, Iterable
+
+
+# https://leetcode.com/problems/valid-sudoku
+def is_valid_sudoku(board: List[List[int]]) -> bool:
+    """
+    >>> is_valid_sudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    ...     , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    ...     , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    ...     , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    ...     , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    ...     , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    ...     , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    ...     , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    ...     , [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
+    True
+    >>> is_valid_sudoku([["8", "3", ".", ".", "7", ".", ".", ".", "."]
+    ... , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    ... , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    ... , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    ... , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    ... , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    ... , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    ... , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    ... , [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
+    False
+    """
+
+    def has_repeat(range: Iterable[tuple[int, int]]) -> bool:
+        encountered = set()
+        for x, y in range:
+            digit = board[x][y]
+            if digit == ".":
+                continue
+            if digit in encountered:
+                return True
+            encountered.add(digit)
+        return False
+
+    rows = cols = 9
+    for r in range(rows):
+        row = [(r, c) for c in range(cols)]
+        if has_repeat(row):
+            return False
+
+    for c in range(cols):
+        col = [(r, c) for r in range(rows)]
+        if has_repeat(col):
+            return False
+
+    for r in range(0, rows, 3):
+        for c in range(0, cols, 3):
+            square = [(x, y) for x in range(r, r + 3) for y in range(c, c + 3)]
+            if has_repeat(square):
+                return False
+
+    return True
+
+
 # https://leetcode.com/problems/alice-and-bob-playing-flower-game
 def flower_game(n: int, m: int) -> int:
     """
